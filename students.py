19eea5e0a8e4cb115ce990e1ad2838ad826a9903
willowcheng <e.wingus@Gmail.com>
2015-05-30 00:28:17 -0400

    Modeling

--- students.py
+++ students.py
@@ -1,2 +1,17 @@
 __author__ = 'willowcheng'
 
+from peewee import *
+
+db = SqliteDatabase('students.db')
+
+class Student(Model):
+    username = CharField(max_length=255, unique=True)
+    points = IntegerField(default=0)
+
+    class Meta:
+        database = db
+
+
+if __name__ == '__main__':
+    db.connect()
+    db.create_tables([Student], safe=True)
