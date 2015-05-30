23ca8a11817c049092b0424d2955813a6075ea5a
willowcheng <e.wingus@Gmail.com>
2015-05-30 16:17:51 -0400

    Doing data entry

--- diary.py
+++ diary.py
@@ -1,27 +1,39 @@
 __author__ = 'willowcheng'
 
+import datetime
 from peewee import *
 
 db = SqliteDatabase('diary.db')
 
+
 class Entry(Model):
-    # content
-    # timestamp
+    content = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = db
 
+def initialize():
+    """Create the database and the table if they don't exist"""
+    db.connect()
+    db.create_tables([Entry], safe=True)
+
 def menu_loop():
     """Show the menu"""
+
 
 def add_entry():
     """Add an entry"""
 
+
 def view_entries():
     """View previous entries"""
+
 
 def delete_entry(entry):
     """Delete an entry"""
 
+
 if __name__ == '__main__':
+    initialize()
     menu_loop()
